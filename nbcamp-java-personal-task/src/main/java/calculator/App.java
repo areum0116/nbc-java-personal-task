185777d2cc171f5d6fa0226cacbc4b739ba7e648
areum0116 <areum0116@naver.com>
2024-07-25 11:29:28 +0900

    level 1 - 3

--- nbcamp-java-personal-task/src/main/java/calculator/App.java
+++ nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -1,8 +1,33 @@
 package calculator;
 
-public class App {
+import java.util.*;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello worrrrrld");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter your second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter operator: ");
+        char operator = sc.next().charAt(0);
+
+        int result = 0;
+        switch (operator) {
+            case '+' -> {
+                result = num1 + num2;
+            }
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if(num2 == 0) {
+                    System.out.println("0 cannot be used as a second parameter in division");
+                    break;
+                }
+                else result = num1 / num2;
+            }
+        }
+        System.out.println(result);
     }
 }
