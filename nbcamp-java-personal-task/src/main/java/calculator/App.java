50f33058eaedd3b49ee7cd341d691b8669011cbc
areum0116 <areum0116@naver.com>
2024-07-25 13:34:50 +0900

    level 2 - 2

--- nbcamp-java-personal-task/src/main/java/calculator/App.java
+++ nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -14,8 +17,12 @@ public class App {
             System.out.print("Enter your second number: ");
             int num2 = sc.nextInt();
 
-
-
+            try{
+                int result = calculator.calculate(num1, operator, num2);
+                System.out.println(result);
+            } catch (BadInputException e) {
+                System.out.println(e.getMessage());
+            }
 
             System.out.print("continue? (Enter 'exit' to exit) : ");
             if(sc.next().equals("exit")) break;
