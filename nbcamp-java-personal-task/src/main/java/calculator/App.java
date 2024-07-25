aa54f61d7b41c826232c182966475d31127f3167
areum0116 <areum0116@naver.com>
2024-07-25 13:23:50 +0900

    level 2 - 1

--- nbcamp-java-personal-task/src/main/java/calculator/App.java
+++ nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -4,11 +4,6 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        int[] arr = new int[10];    // Store the answer of the operations using array.
-        int index = 0;      // Store array's last index
-
-        Queue<Integer> queue = new LinkedList<>();  // Store the answer of the operations using JCF.
-
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -19,29 +14,6 @@ public class App {
             System.out.print("Enter your second number: ");
             int num2 = sc.nextInt();
 
-            int result = 0;
-
-            switch (operator) {
-                case '+' -> result = num1 + num2;
-                case '-' -> result = num1 - num2;
-                case '*' -> result = num1 * num2;
-                case '/' -> {
-                    if (num2 == 0) System.out.println("0 cannot be used as a second parameter in division");
-                    else result = num1 / num2;
-                }
-            }
-            System.out.println("result : " + result);
-
-            // Storing answer using array
-            if(index == 10) {
-                int[] tmp = arr.clone();
-                for (int i = 1; i < 10; i++) {
-                    arr[i-1] = tmp[i];
-                }
-                arr[9] = result;
-            }
-            else
-                arr[index++] = result;
 
             // Storing answer using JCF
             queue.add(result);
--- nbcamp-java-personal-task/src/main/java/calculator/Calculator.java
+++ nbcamp-java-personal-task/src/main/java/calculator/Calculator.java
@@ -1,5 +1,22 @@
 package calculator;
 
-public class Calculator {
+import java.util.*;
 
+public class Calculator {
+    private Queue<Integer> queue = new LinkedList<>();  // Store the answer of the operations using JCF.
+
+    public int calculate(int num1, char operator, int num2) throws BadInputException{
+        int result = 0;
+
+        switch (operator) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if (num2 == 0) throw new BadInputException("0 cannot be used as a second parameter in division");
+                else result = num1 / num2;
+            }
+        }
+        return result;
+    }
 }
