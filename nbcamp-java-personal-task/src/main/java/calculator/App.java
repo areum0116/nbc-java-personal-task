fb130753cd52aee66cef5a1b5b5957cc3d9be815
areum0116 <areum0116@naver.com>
2024-07-25 11:36:33 +0900

    level 1 - 4

--- nbcamp-java-personal-task/src/main/java/calculator/App.java
+++ nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -6,28 +6,28 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your first number: ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter your second number: ");
-        int num2 = sc.nextInt();
-        System.out.print("Enter operator: ");
-        char operator = sc.next().charAt(0);
+        while (true) {
+            System.out.print("Enter your first number: ");
+            int num1 = sc.nextInt();
+            System.out.print("Enter operator: ");
+            char operator = sc.next().charAt(0);
+            System.out.print("Enter your second number: ");
+            int num2 = sc.nextInt();
 
-        int result = 0;
-        switch (operator) {
-            case '+' -> {
-                result = num1 + num2;
-            }
-            case '-' -> result = num1 - num2;
-            case '*' -> result = num1 * num2;
-            case '/' -> {
-                if(num2 == 0) {
-                    System.out.println("0 cannot be used as a second parameter in division");
-                    break;
+            int result = 0;
+
+            switch (operator) {
+                case '+' -> result = num1 + num2;
+                case '-' -> result = num1 - num2;
+                case '*' -> result = num1 * num2;
+                case '/' -> {
+                    if (num2 == 0) System.out.println("0 cannot be used as a second parameter in division");
+                    else result = num1 / num2;
                 }
-                else result = num1 / num2;
             }
+            System.out.println("result : " + result);
+            System.out.print("continue? (Enter exit to exit) : ");
+            if(sc.next().equals("exit")) break;
         }
-        System.out.println(result);
     }
 }
